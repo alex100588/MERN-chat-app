1248dc00f67a3cdfd98e7ac8b6eff0aae64270d5
alex100588 <alexandru_ionasc@yahoo.com>
2025-04-08 13:44:26 +0300

    logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -81,5 +81,11 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  console.log("logout");
+  try {
+    res.cookie("jwt", "",{ maxAge: 0 })
+    res.status(200).json({message: "Logged out successfully"})
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
